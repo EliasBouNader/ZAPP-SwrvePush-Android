e61e7a0f13fd8383df3a278a0512336004dcf06c
elias.bounader <6Ee?6se_7>
2020-04-24 15:36:37 +0300

    Swrve Push plugin 0.0.5

---
+++ swrvepush-android/src/main/java/com/sportsmax/swrvepush_android/Constant.kt
@@ -0,0 +1,6 @@
+package com.sportsmax.swrvepush_android
+
+
+const val SWRVE_ACCOUNT_ID = "swrve_account_id"
+const val SWRVE_SANDBOX_KEY = "swrve_sandbox_key"
+const val SWRVE_PRODUCTION_KEY = "swrve_production_key"
---
+++ swrvepush-android/src/main/java/com/sportsmax/swrvepush_android/PushProvider.kt
@@ -0,0 +1,89 @@
+package com.sportsmax.swrvepush_android
+
+import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.content.pm.ApplicationInfo
+import android.os.Build
+import android.util.Log
+import com.applicaster.plugin_manager.push_plugin.PushContract
+import com.applicaster.plugin_manager.push_plugin.helper.PushPluginsType
+import com.applicaster.plugin_manager.push_plugin.listeners.PushTagLoadedI
+import com.applicaster.plugin_manager.push_plugin.listeners.PushTagRegistrationI
+import com.swrve.sdk.SwrveNotificationConfig
+import com.swrve.sdk.SwrveSDK
+import com.swrve.sdk.config.SwrveConfig
+
+class PushProvider : PushContract {
+    override fun registerPushProvider(
+        context: Context,
+        registerID: String
+    ) {
+        initPushProvider(context)
+    }
+
+    override fun initPushProvider(context: Context) {
+        val config = SwrveConfig()
+        var channel: NotificationChannel? = null
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            channel = NotificationChannel(
+                "123",
+                "Devapp swrve default channel",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            if (context.getSystemService(Context.NOTIFICATION_SERVICE) != null) {
+                val notificationManager =
+                    context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+                notificationManager.createNotificationChannel(channel)
+            }
+        }
+        val notificationConfig = SwrveNotificationConfig.Builder(
+            R.drawable.ic_player_logo,
+            R.drawable.ic_player_logo,
+            channel
+        )
+
+        config.notificationConfig = notificationConfig.build()
+
+
+        val appId = PluginConfigurationHelper.getConfigurationValue(SWRVE_ACCOUNT_ID)?.toInt() ?: 0
+        val isDebuggable = 0 != context.applicationInfo.flags and ApplicationInfo.FLAG_DEBUGGABLE
+
+        val apiKey = if (isDebuggable){
+            PluginConfigurationHelper.getConfigurationValue(SWRVE_SANDBOX_KEY)
+        }else{
+            PluginConfigurationHelper.getConfigurationValue(SWRVE_PRODUCTION_KEY)
+        }
+
+        config.setNotificationListener { pushJson ->
+            Log.wtf("Received push", "of body: " + pushJson.toString(1))
+        }
+        SwrveSDK.createInstance(context.applicationContext as Application?, appId, apiKey, config)
+    }
+
+    override fun setPluginParams(params: Map<*, *>?) {}
+    override fun addTagToProvider(
+        context: Context,
+        tag: List<String>,
+        pushTagRegistrationListener: PushTagRegistrationI
+    ) {
+    }
+
+    override fun removeTagToProvider(
+        context: Context,
+        tag: List<String>,
+        pushTagRegistrationListener: PushTagRegistrationI
+    ) {
+    }
+
+    override fun getPluginType(): PushPluginsType {
+        return PushPluginsType.applicaster
+    }
+
+    override fun getTagList(
+        context: Context,
+        listener: PushTagLoadedI
+    ) {
+    }
+}
